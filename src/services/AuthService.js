df80a23b4400628a7ee5c8821b2b3f62b2eab27f
fabio <fabio@goelditechnology.ch>
2022-10-14 11:57:15 +0200

    added secure connect connection infos

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -5,7 +5,7 @@ export default (function () {
         var OIDC_DOMAIN = process.env.VUE_APP_AUTH_HOST;
         var settings = {
             userStore: new oidc_client.WebStorageStateStore({ store: window.localStorage }),
-            // authority: 'x', // OIDC_DOMAIN,
+            authority: 'x', // OIDC_DOMAIN,
             client_id: process.env.VUE_APP_AUTH_CLIENT_ID,
             redirect_uri: process.env.VUE_APP_AUTH_REDIRECT_URL,
             response_type: "token id_token",
@@ -13,15 +13,15 @@ export default (function () {
             post_logout_redirect_uri: "http://localhost:8080/",
             // filterProtocolClaims: false,
             loadUserInfo: false,
-            signingKeys: [{"use":"sig","kty":"RSA","kid":"423051494292503418","alg":"RS256","n":"wHMkz31GStrLs7ATyksNlOvE8H0QMGnIZD4F1YCKc97IFIYCa9oqDns1UqtoSkelet_d0keegyISdaqjeGCV2CTldJ1QlkI4WfYstePSgkyS97ypVafzvJdHZR9_aBfQmi-_wo4t71TnqZ8dbO0mKm0uPq6C0-KUa_o4F4Pi3895rLNcY9b5CMs2hV9LYkAnBWnSYtaaZFqJWeshTquClkcif2wOuEKlUdgJEaQdGwkB7yc6JCAICzL5tejNqhX7L-LfgmPDxeV65KsONsqHiXeVpwW0Y2G5Z3xpd8toZrSkpPjyEF4mLkzPW4K9HzrocdFHegvbxt64dSctMZFG8Q","e":"AQAB"}],
             metadata: {
-                issuer: OIDC_DOMAIN,
+                issuer: OIDC_DOMAIN + '/',
                 //audience: "http://localhost:3005",
                 authorization_endpoint: OIDC_DOMAIN + "/authorize",
                 // userinfo_endpoint: OIDC_DOMAIN + "/me",
                 end_session_endpoint: OIDC_DOMAIN + "/logout",
                 jwks_uri: OIDC_DOMAIN + "/keys",
-            }
+            },
+            signingKeys: [{"use":"sig","kty":"RSA","kid":"423051494292503418","alg":"RS256","n":"wHMkz31GStrLs7ATyksNlOvE8H0QMGnIZD4F1YCKc97IFIYCa9oqDns1UqtoSkelet_d0keegyISdaqjeGCV2CTldJ1QlkI4WfYstePSgkyS97ypVafzvJdHZR9_aBfQmi-_wo4t71TnqZ8dbO0mKm0uPq6C0-KUa_o4F4Pi3895rLNcY9b5CMs2hV9LYkAnBWnSYtaaZFqJWeshTquClkcif2wOuEKlUdgJEaQdGwkB7yc6JCAICzL5tejNqhX7L-LfgmPDxeV65KsONsqHiXeVpwW0Y2G5Z3xpd8toZrSkpPjyEF4mLkzPW4K9HzrocdFHegvbxt64dSctMZFG8Q","e":"AQAB"}]
         };
         this.userManager = new oidc_client.UserManager(settings);
     }
