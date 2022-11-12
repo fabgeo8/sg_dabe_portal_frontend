254221af04d93e947962a8be60e5042fa5e07dc7
fabio <fabio@goelditechnology.ch>
2022-11-12 09:56:48 +0100

    added user management

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -5,12 +5,16 @@ const state = {
     isMunicipalityUser: true,
     userMunicipality: '',
     isAuthorized: null,
+    lastRequestTimestamp: null,
     activeSettingMunicipality: '',
     activeClient: 'Kanton',
     isAdmin: true,
     AccessKey: '',
     isLoggedIn: false
 }
+
+// expiration of user info in sec
+const userInfoExpiration = 5
 
 const getters = {
     getUserMunicipality (state) {
@@ -39,15 +43,26 @@ const getters = {
 
     getActiveSettingMunicipality(state) {
         return state.activeSettingMunicipality !== 'canton'
+    },
+
+    getUserInfoIsExpired (state) {
+        if (state.lastRequestTimestamp === null) {
+            return true
+        } else {
+            if (new Date().getMilliseconds() - state.lastRequestTimestamp.getMilliseconds() > userInfoExpiration * 1000) {
+                console.log("user info expired, renewing...")
+                return true
+            }
+        }
+        return false
     }
 }
 
 const actions = {
-
     getUserApiInfo ({ commit, state }) {
-
         // get user info if info is not set
-        if (!state.isAuthorized) {
+        // also get info if user info request is expired
+        if (!state.isAuthorized || getters.getUserInfoIsExpired()) {
             console.log("get user info from api")
             axios.get('users/me', )
                 .then((res) => {
