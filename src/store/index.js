962f1a24c3f1ee2411baa3098000344282ae39e8
fabio <fabio@goelditechnology.ch>
2022-11-24 08:55:30 +0100

    added association to activities

--- src/store/index.js
+++ src/store/index.js
@@ -9,7 +9,7 @@ Vue.use(Vuex)
 
 const vuexLocalStorage = new VuexPersist({
   key: 'vuex', // The key to store the state on in the storage provider.
-  storage: window.localStorage, // or window.sessionStorage or localForage
+  storage: window.sessionStorage, // or window.sessionStorage or localForage
   // Function that passes the state and returns the state with only the objects you want to store.
   reducer: state => ({
     data: {
